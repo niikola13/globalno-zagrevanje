5c79aca84d80222e582114edbc29dc6aff01f2de
niikola13 <gale.vh@gmail.com>
2025-04-25 12:20:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function prikaziPoruku() {
+    const poruka = document.getElementById("poruka");
+    poruka.innerText = "Образовање, сарадња и активизам су кључни у борби против климатских промена. Свако може дати свој допринос!";
+}
